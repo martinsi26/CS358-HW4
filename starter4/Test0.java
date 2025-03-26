dea89809de07783e8736f957990d3b1ba080b5a3
martinsi26 <martinsi26@up.edu>
2025-03-25 19:56:47 -0700

    c

--- starter4/Test0.java
+++ starter4/Test0.java
@@ -1,15 +1,5 @@
-class Parent {
-    int x;
-}
-
-class Child extends Parent {
-    String y;
-}
-
 class Main {
     public void main() {
-        Child e = new Child();
-
-        int value = e.x; 
+        
     }
 }
